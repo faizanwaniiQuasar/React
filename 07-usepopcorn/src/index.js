cf079eb9c647aa5a3934db0e2b4a1516c7fc3826
faizanwaniiQuasar <faizan.wani@iquasar.com>
2023-08-21 14:09:15 +0530

    add:added star component

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StartRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={5} />
+    <StarRating size={24} color="red" />
   </React.StrictMode>
 );
 
