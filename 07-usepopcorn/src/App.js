181efefb18489d0a6a0db241f0abd22f7ffe87f5
faizanwaniiQuasar <faizan.wani@iquasar.com>
2023-08-25 16:21:17 +0530

    add fetch cleanup

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -73,12 +73,14 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
           if (!res.ok)
             throw new Error("Something went wrong with fetching movies");
@@ -87,9 +89,13 @@ export default function App() {
           if (data.Response === "False")
             throw new Error("Sorry Movie not found");
           setMovies(data.Search);
+          setError("");
         } catch (err) {
           // console.log(err.message);
-          setError(err.message);
+
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -102,6 +108,9 @@ export default function App() {
       }
 
       fetchMovies();
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
